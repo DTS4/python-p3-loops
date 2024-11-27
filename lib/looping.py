f17fa5e261bd6982ef881d1e7823e9907a7bd4f5
Keith <keith.githinji@student.moringaschool.com>
2024-11-27 09:21:03 +0300

    Completed the lab

--- lib/looping.py
+++ lib/looping.py
@@ -14,20 +14,14 @@ def square_integers(int_list):
     # code goes here!
     pass
 
-def fizzbuzz_printer():
+def fizzbuzz():
     for num in range(1, 101):
-        print(fizzbuzz(num))
-
-def fizzbuzz(num):
-    if num % 3 == 0 and num % 5 == 0:
-        return "FizzBuzz"
-    elif num % 3 == 0:
-        return "Fizz"
-    elif num % 5 == 0:
-        return "Buzz"
-    else:
-        return num
-
-fizzbuzz_printer()
-
- 
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+    pass
